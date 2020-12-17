04be45e1cc3d98d95ef2c1850694f49e02767319
shibayaman <shibayaman7374@gmail.com>
2020-12-18 02:26:55 +0900

    Add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import dotenv from 'dotenv';
 import { createConnection } from 'typeorm';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schemas';
 
 dotenv.config();
 
-createConnection()
-  .then(async connection => {
-    console.log('code starts here');
-  })
-  .catch(err => {
-    console.error(err);
+const startServer = async () => {
+  const connection = await createConnection();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
   });
+
+  const app = express();
+
+  server.applyMiddleware({ app });
+
+  const port = process.env.PORT || 4000;
+  app.listen({ port }, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
+};
+
+startServer().catch(err => console.error(err));
